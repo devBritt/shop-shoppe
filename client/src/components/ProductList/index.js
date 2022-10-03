f804db5ea09f0fe160d2d08359a4d04a1ba80916
Britt Carlton <brittanyrc95@gmail.com>
2022-10-03 15:51:16 -0400

    Implement persistent storage of products in indexedDB

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -7,6 +7,7 @@ import spinner from '../../assets/spinner.gif';
 
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -19,8 +20,21 @@ function ProductList() {
             type: UPDATE_PRODUCTS,
             products: data.products
         });
+
+        data.products.forEach((product) => {
+            idbPromise('products', 'put', product);
+        });
+    } else if (!loading) {
+        // since we're offline, get all of the data from the 'products' store
+        idbPromise('products', 'get').then((products) => {
+            // use retrieved data to set global state for offline browsing
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: products
+            });
+        });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
